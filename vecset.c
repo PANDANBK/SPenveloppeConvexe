dc00b3d0c1d223fe61796977c22d43ef79253f1d
Smiley32 <jeannin.emile@laposte.net>
2017-03-07 12:17:12 +0100

    Ajout de vecset_add

--- vecset.c
+++ vecset.c
@@ -21,7 +21,20 @@ void vecset_destroy(struct vecset *self)
 
 void vecset_add(struct vecset *self, struct vec p)
 {
+  assert(self != NULL);
 
+  if(self->size == self->capacity)
+  {
+    size_t newCapacity = self->capacity * 2;
+    struct vec *newArray = malloc(newCapacity * sizeof(struct vec));
+    memcpy(newArray, self->data, self->size * sizeof(struct vec));
+    free(self->data);
+    self->data = newArray;
+    self->capacity = newCapacity;
+  }
+
+  self->data[self->size] = p;
+  self->size++;
 }
 
 const struct vec *vecset_max(const struct vecset *self, comp_func_t func, const void *ctx)
